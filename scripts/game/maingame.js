8b85a62968f245aeabf6d04d5e3351b59a6cddb7
knoddin <knoddin@gmail.com>
2016-09-07 16:42:46 -0400

    game - index.html and maingame.js
    added working functions for toggling turns!

--- scripts/game/maingame.js
+++ scripts/game/maingame.js
@@ -3,7 +3,8 @@
 let playerX = 1;
 let playerO = 2;
 
-function displayLetter(box){
+let displayLetter = function(box){
+  // prevent.default();
   if (playerX == 1) {
     document.getElementById(box).innerHTML = "X";
     playerX = 0;
